e9baefe4ea35442ade47748ae5b18e335b16d619
Jan Pieter Waagmeester <jieter@jieter.nl>
2017-09-27 13:11:55 +0200

    Make changing values actually do something in the browser interface, minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
-var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./index.js",
+  entry: './browser.js',
   output: {
     path: __dirname,
     filename: "bundle.js"
   },
-  // module: {
-  //     loaders: [
-  //         { test: /\.js$/, loader: 'babel-loader', query: { presets: ['es2015'] } },
-  //     ]
-  // },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
+  module: {
+      loaders: [
+          { test: /\.js$/, loader: 'babel-loader', query: { presets: ['es2015'] } },
+      ]
+  },
+  plugins: [
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };
